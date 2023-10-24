94b4607dfd0a00fa792a050a29d37310e406d8be
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2023-10-24 13:59:58 +0700

    Test IO

--- src/main/java/com/mycompany/train_ticket/Train_ticket.java
+++ src/main/java/com/mycompany/train_ticket/Train_ticket.java
@@ -4,13 +4,32 @@
 
 package com.mycompany.train_ticket;
 
+import com.mycompany.train_ticket.controller.IOSystem;
+import com.mycompany.train_ticket.model.Customer;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author LENOVO
  */
 public class Train_ticket {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("Hello World!");
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer(1,"Nguyen Van A",new Date(2000,1,1),"Vip"));
+        String path = System.getProperty("user.dir") + "/src/Files/customer.dat";
+        System.out.println(path);
+
+        IOSystem.getInstance().write( customers , path);
+        List<Customer> customers1 = IOSystem.getInstance().read(path);
+        customers1.forEach(c -> System.out.println(c.getFullName()));
     }
 }
