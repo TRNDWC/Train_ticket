f4b710548fdf77e3c923290c302da68ee87624d4
TRNDWC <tranductdn@gmail.com>
2023-10-24 11:23:29 +0700

    first commit

---
+++ src/main/java/com/mycompany/train_ticket/Train_ticket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.train_ticket;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Train_ticket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
